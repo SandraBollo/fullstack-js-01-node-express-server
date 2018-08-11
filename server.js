74ac71baceab69a6cd9057e6fb66e73547f683e4
SandraBollo <sandra.bollo.b@gmail.com>
2018-08-11 18:56:27 -0500

    server-router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 ///importar  express
 const express = require('express')
 const pageRouter = require ('./src/routes/pageRouter.js')
+const apiRouter = require('./src/routes/apiRouter')
 ///////2
 
 const app = express()
@@ -12,6 +13,7 @@ const PORT = 3000
 
 
 app.use('/', pageRouter)
+app.use('/api', apiRouter)
 
 
 
