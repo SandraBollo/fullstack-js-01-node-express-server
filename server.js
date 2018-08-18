e4e28365189f7d8f8de650f6869424c4abfa6465
SandraBollo <sandra.bollo.b@gmail.com>
2018-08-18 17:27:39 -0500

    cool?

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 ///importar  express
+
 const express = require('express')
+const ejs = require ('ejs')
+
+// importar knex
+const knex = require('knex')
+
+// importar knexfile.js
+const devDbConfig = require('./knexfile.js')
+
+
+
 const pageRouter = require ('./src/routes/pageRouter.js')
 const apiRouter = require('./src/routes/apiRouter')
+
 ///////2
 
 const app = express()
+
+
+// configuracion data access
+const appDb = Knex(devDbConfig.development)
+app.locals.db = appDb
 
 //////indicar el puerto
 
@@ -12,13 +29,23 @@ const PORT = 3000
 
 
 
+app.use(express.static( `${__dirname}/public`) )
+
+app.engine( 'ejs', ejs.renderFile )
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/src/views`)
+
 app.use('/', pageRouter)
 app.use('/api', apiRouter)
 
 
-
+app.use((req, res )=>{
+  res.send('<h3> 404 BIG ERROR - PAGE NO EXIST </h3>')
+})
 
  ///pso 3
+
+
  app.listen(PORT,()=>{
    console.log(`App listening on localhost:${PORT}`);
  })
